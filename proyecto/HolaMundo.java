fed189ee7c5966b2178118d86c90e6cac5b3fd57
Gabriel Namendy <elbriganamendygarcia@gmail.com>
2025-06-25 10:03:30 -0600

    Mensaje de confirmacion 2

--- proyecto/HolaMundo.java
+++ proyecto/HolaMundo.java
@@ -4,8 +4,8 @@ public class HolaMundo {
 public static void main(String[] args) {
 
 String nombre = "Estudiante";
-int a = 5;
-int b = 3;
+int a = 8;
+int b = 2;
 int suma = a + b;
 System.out.println("Hola, " + nombre + "!");
 System.out.println("La suma de " + a + " + " + b + " es: " + suma);
